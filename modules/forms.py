3a998b94e30ee1677180d291e273356f3b01fe07
Kolinko-Danylo <kolinko@ucu.edu.ua>
2018-10-16 11:25:42 +0300

    Added documentation

--- modules/forms.py
+++ modules/forms.py
@@ -3,11 +3,13 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 class SearchForm(FlaskForm):
+    """Search lyrics form."""
     author_name = StringField('Musician', validators=[DataRequired()])
     song_name = StringField('Song', validators=[DataRequired()])
     submit = SubmitField('Search!')
 
 
 class WordsForm(FlaskForm):
+    """Forming terms set form"""
     words = StringField('Words', validators=[DataRequired()])
     submit = SubmitField('Submit')
